743d20b8aa02f396606fe920c3c155902d88d472
OlegPlugaru <olegplugaru.md@gmail.com>
2024-02-20 20:26:39 +0200

    Initial Commit

---
+++ DynamicForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicForm
+//
+//  Created by Oleg Plugaru on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
